5bf1f03c9120c76319009d5a637f8c919be6dd79
Brandon <7310114+munyb@users.noreply.github.com>
2022-03-27 21:31:32 -0700

    Pep8 auto syntax

--- database_scripts/update_event_artist.py
+++ database_scripts/update_event_artist.py
@@ -6,54 +6,55 @@ from models import *
 from peewee import *
 from playhouse.shortcuts import model_to_dict, dict_to_model
 from utilities.timer import Timer
-from showfeur_db import ShowfeurDB
+from firehouse_db_service import FirehouseDBService
+
 
 async def main():
-    timer = Timer('')
+    timer = Timer("")
     timer.begin()
-    
+
     current_date = date.today()
-    db_service = ShowfeurDB()
+    db_service = FirehouseDBService()
     events_query = Events.select().where((Events.event_date <= current_date))
     event_ids = [event.event_id for event in events_query]
-    event_artist_query = (Event_Artist
-                          .select(Event_Artist.event_id)
-                          .where((Event_Artist.event_id
-                                  .in_(event_ids))))
+    event_artist_query = Event_Artist.select(Event_Artist.event_id).where((Event_Artist.event_id.in_(event_ids)))
     active_events = [event for event in events_query]
     existing_event_artist_records = [event_artist.event_id for event_artist in event_artist_query]
     existing_event_artist_event_ids = [event.event_id for event in existing_event_artist_records]
     event_artist_pairs_to_create = []
-    
+
     for event in active_events:
-        if event.event_id not in existing_event_artist_event_ids:   
+        if event.event_id not in existing_event_artist_event_ids:
             event_name = event.event_name
             artist_name = None
 
-            if 'with' in event_name:
-                artist_name = event_name.split(' with')[0]
-            elif 'and' in event_name:
-                artist_name = event_name.split(' and')[0]
+            if "with" in event_name:
+                artist_name = event_name.split(" with")[0]
+            elif "and" in event_name:
+                artist_name = event_name.split(" and")[0]
             else:
-                artist_name = event_name.split(' at')[0]
-                
+                artist_name = event_name.split(" at")[0]
+
             artist_query = Artists.select().where(Artists.artist_name == artist_name)
             artists = [model for model in artist_query]
-            
+
             if len(artists) > 0:
                 artist_id = [artist.artist_id for artist in artists][0]
-                event_artist_pairs_to_create.append({ 
-                    'event_id': event.event_id,
-                    'artist_id': artist_id,
-                    'is_headliner': True,
-                })
-                        
+                event_artist_pairs_to_create.append(
+                    {
+                        "event_id": event.event_id,
+                        "artist_id": artist_id,
+                        "is_headliner": True,
+                    }
+                )
+
     if len(event_artist_pairs_to_create) > 1:
         db_service.save_event_artists(event_artist_pairs_to_create)
-        
+
     timer.stop()
     print(timer.results())
     timer.reset()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
--- main.py
+++ main.py
@@ -7,17 +7,17 @@ import logging
 import pandas as pd
 import datetime
 from utilities.pretty_print import pretty_print as pp
-from showfeur_db import ShowfeurDB
 from spotify.spotify_service import SpotifyService
-import songkick_event_import
+import run_event_import
 
 # TODO: Write script to pull down playlists
 # tthen extractt artists from playlists
 # filter for artists which do not already exist
 # get genres for each artistt
 # create models for artists to add
-# create artist genres 
+# create artist genres
 # save all models
+
 
 def save_artists_to_csv(data):
     artist_names = set()
@@ -26,79 +26,74 @@ def save_artists_to_csv(data):
         artist_names.update(artists)
     artist_list = list(artist_names)
     # pp(list(artist_names), True)
-    
-    filename = f'artist_output_{datetime.date.today()}.csv'
-    root = 'spotify/artist_exports'
-    path = root + '/' + filename
-    headers = ['artists']
+
+    filename = f"artist_output_{datetime.date.today()}.csv"
+    root = "spotify/artist_exports"
+    path = root + "/" + filename
+    headers = ["artists"]
     df = pd.DataFrame(artist_list, columns=headers)
     df.to_csv(path, index=False)
     df_saved_file = pd.read_csv(path)
-    print(f'artists saved to {path}')
-    
+    print(f"artists saved to {path}")
+
+
 async def spotify_menu():
     auth_token = input("Enter auth token:\n")
     service = SpotifyService(auth_token)
-    
+
     # TODO: Make a dummy call to check if auth token is valid, if not ask user to input again.
     while True:
         print("\nChoose one of the following actions by entering it's respective number.")
         print("\t1 - Get my playlists")
         print("\t0 - Exit to main menu")
         choice = int(input("\tInput: "))
-        
+
         if choice == 1:
             while True:
                 print("\nFetching playlists...")
                 playlists = await service.get_my_playlists()
-                
+
                 if not len(playlists) > 0:
-                    print('No playlists found!')
+                    print("No playlists found!")
                 else:
                     pl_keys = playlists.keys()
                     pl_direct = {}
                     for index, key in enumerate(pl_keys):
                         pl_direct[f"{index}"] = key
-                        print(f'\t{index}: {playlists[key]}')
-                        
+                        print(f"\t{index}: {playlists[key]}")
+
                     while True:
-                        sel_pl = input("Select one of the above playlists to output artists: ") 
-                        if (int(sel_pl) >= len(pl_direct)):
-                            print('invalid input...')
-                        else: 
+                        sel_pl = input("Select one of the above playlists to output artists: ")
+                        if int(sel_pl) >= len(pl_direct):
+                            print("invalid input...")
+                        else:
                             response = service.get_playlist(pl_direct[sel_pl])
-                            #TODO: Filter remixed songs and find way to select correct artist to
+                            # TODO: Filter remixed songs and find way to select correct artist to
                             #      prevent non-house artists from being added
                             playlist_artists = save_artists_to_csv(response)
                             return False
         elif choice == 0:
             return False
-    
+
 
 async def main():
-    logging.basicConfig(
-        filename='Run.log', 
-        level=logging.INFO, 
-        format='%(asctime)s:: %(message)s'
-    )
+    logging.basicConfig(filename="Run.log", level=logging.INFO, format="%(asctime)s:: %(message)s")
     print("Welcome to Showfeur's backend CLI.")
-    
+
     while True:
         service_choice = int(input("\nPlease choose a service.\n\t1 - Songkick\n\t2 - Spotify\n\t0 - Exit\nInput: "))
-        print('')
+        print("")
         if service_choice == 1:
-            songkick_event_import.main()
-            #print("Sorry, not developed yet!")
-            
+            run_event_import.main()
+            # print("Sorry, not developed yet!")
+
         elif service_choice == 2:
             await spotify_menu()
         elif service_choice == 0:
             exit()
         else:
-            print('choice not recognized!')
-    
-    
+            print("choice not recognized!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
