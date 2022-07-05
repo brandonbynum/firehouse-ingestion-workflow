8eb3f569e17dc3cbb3c3db48f181e9ba337a9d6e
Brandon <7310114+brandonbynum@users.noreply.github.com>
2022-07-04 17:04:06 -0700

    Error handling + better output descriptions

--- utilities/insert_events.py
+++ utilities/insert_events.py
@@ -3,29 +3,31 @@ from models import Event_Artist, Events, pg_db
 def insert_events(event_details: dict):
     event_artist_models = []
     
-    #TODO: Exception handling so one failure doesn't hold up reest of inserts
     for key in event_details.keys():
         event_to_import = event_details[key]
-        
-        with pg_db.atomic():
-            event_insert = Events.get_or_create(
-                date=event_to_import["date"],
-                venue_id=event_to_import["venue_id"],
-                name=event_to_import["name"],
-                tickets_link=event_to_import["ticket_url"],
-                defaults={
-                    "type": "Concert",
-                    "is_active": True,
-                    "start_at": event_to_import["start_at"],
-                    "end_at": None
-                }
-            )
-        print(event_details[key])
-            
-        event_artist_models.append({
-            "artist_id": key,
-            "event_id": event_insert[0].id,
-        })
+        try:
+            with pg_db.atomic():
+                event_insert = Events.get_or_create(
+                    date=event_to_import["date"],
+                    venue_id=event_to_import["venue_id"],
+                    name=event_to_import["name"],
+                    tickets_link=event_to_import["ticket_url"],
+                    defaults={
+                        "type": "Concert",
+                        "is_active": True,
+                        "start_at": event_to_import["start_at"],
+                        "end_at": None
+                    }
+                )
+            insert_status = "successful insert" if event_insert[1] else "record already exists"
+            print(f"\n{event_details[key]}\n\t{insert_status}")
+                
+            event_artist_models.append({
+                "artist_id": key,
+                "event_id": event_insert[0].id,
+            })
+        except Exception as e:
+            print(f"Failed to insert the following event\n\t{event_details[key]}")
         
     # TODO (Performance): Add Unique Constraint to event_artist table and insert using 'insert_many'
     for model in event_artist_models:
@@ -35,4 +37,5 @@ def insert_events(event_details: dict):
                 event_id=model["event_id"],
                 defaults={"headliner": True}
             )
-            print("\n", model, event_artist_insert[1])
+        insert_status = "successful insert" if event_artist_insert[1] else "record already exists"
+        print(f"\n{model} --> {insert_status}")
